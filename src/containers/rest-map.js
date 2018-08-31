7603bb650f9155765d4984c89d1b53a7655e4ad6
ethankpow <ethankpow@gmail.com>
2018-08-31 09:42:04 -0400

    added marker to maps and can get current location but need to connect those two

--- src/containers/rest-map.js
+++ src/containers/rest-map.js
@@ -1,9 +1,19 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import GoogleMapReact, {Marker} from 'google-map-react';
+import GoogleMapReact from 'google-map-react';
 
 // import {Map, Marker, Popup, TileLayer} from 'react-leaflet'
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Marker = ({ text }) => <div style={{
+  color: 'white',
+  background: 'grey',
+  padding: '15px 10px',
+  display: 'inline-flex',
+  textAlign: 'center',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: '100%',
+  transform: 'translate(-50%, -50%)'
+}}>{text}</div>;
 
 
 class RestMap extends Component {
@@ -36,14 +46,19 @@ class RestMap extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '75vh', width: '75%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyBkCxcdQOwxQovx01IjtciRbvZtE-XKmV4" }}
+          bootstrapURLKeys={{ key: "AIzaSyB-w6uVNO3Cs4EMkSvEojoqeyHnTXOvbQU" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
 
           <Marker
             name={'Dolores park'}
-            position={{lat: 37.759703, lng: -122.428093}} />
+            position={{
+              lat: this.lat,
+              lng: this.lng,
+              text:{"HI"}
+
+            }} />
 
           </GoogleMapReact>
       </div>
